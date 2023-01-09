9665cd02c522deb59e447ebcef3ea1503084050d
germano-beep <garthur83@gmail.com>
2023-01-09 14:13:38 -0300

    refatorar em qual views as funções devem estar

--- receitas/urls.py
+++ receitas/urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name = 'index'),
-   path('<int:receita_id>', views.receita, name = 'receita'),
-   path('busca', views.busca, name = 'busca')
+  path('', views.index, name = 'index'),
+  path('<int:receita_id>', views.receita, name = 'receita'),
+  path('busca', views.busca, name = 'busca'),
+  path('dashboard', views.dashboard, name='dashboard'),
+  path('cria/receita', views.cria_receita, name='cria_receita'),
+  path('deleta_receita/<int:receita_id>', views.deleta_receita, name='deleta_receita'),
+  path('edita_receita/<int:receita_id>', views.edita_receita, name='edita_receita'),
+  path('atualiza_receita', views.atualiza_receita, name='atualiza_receita'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
-from receitas.models import Receita
 
 # Create your views here.
 def cadastro(request):
@@ -63,69 +62,8 @@ def logout(request):
   auth.logout(request)
   return redirect('index')
 
-def dashboard(request):
-  if request.user.is_authenticated:
-    id = request.user.id
-    receitas = Receita.objects.order_by('-date_receita').filter(pessoa=id)
-
-    dados = {
-      'receitas': receitas
-    }
-
-    return render(request, 'receitas/dashboard.html', dados)
-  else:
-    return redirect('index')
-
-def cria_receita(request):
-  if request.method == 'POST':
-    nome_receita = request.POST['nome_receita']
-    ingredientes = request.POST['ingredientes']
-    modo_preparo = request.POST['modo_preparo']
-    tempo_preparo = request.POST['tempo_preparo']
-    rendimento = request.POST['rendimento']
-    categoria = request.POST['categoria']
-    foto_receita = request.FILES['foto_receita']
-
-    user = get_object_or_404(User, pk=request.user.id)
-    receita = Receita(pessoa=user, nome_receita=nome_receita, ingredientes=ingredientes, 
-                      modo_preparo=modo_preparo, tempo_preparo=tempo_preparo, rendimento=rendimento, 
-                      categoria=categoria, foto_receita=foto_receita)
-
-    receita.save()
-    return redirect('dashboard')
-
-  return render(request, 'receitas/cria_receita.html')
-
-def deleta_receita(request, receita_id):
-  receita = get_object_or_404(Receita, pk=receita_id)
-  receita.delete()
-  return redirect('dashboard')
-
-def edita_receita(request, receita_id):
-  receita = get_object_or_404(Receita, pk=receita_id)
-  receita_a_editar = { 'receita': receita }
-  return render(request, 'receitas/edita_receita.html', receita_a_editar)
-
 def empty_field(field):
   return field.strip()
 
 def different_passwords(password, password2):
   return password != password2
-
-def atualiza_receita(request):
-  if request.method == 'POST':
-    receita_id = request.POST['receita_id']
-    r = get_object_or_404(Receita, pk=receita_id)
-    r.nome_receita = request.POST['nome_receita']
-    r.ingredientes = request.POST['ingredientes']
-    r.modo_preparo = request.POST['modo_preparo']
-    r.tempo_preparo = request.POST['tempo_preparo']
-    r.rendimento = request.POST['rendimento']
-    r.categoria = request.POST['categoria']
-
-    if 'foto_receita' in request.FILES:
-      r.foto_receita = request.FILES['foto_receita']
-
-    r.save()
-
-    return redirect('dashboard')
