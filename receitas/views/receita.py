d7621e7b181a59ddb997e81bc1d7d2b8d4c5a3ab
germano-beep <garthur83@gmail.com>
2023-01-10 12:12:25 -0300

    incluir paginacao no index

--- receitas/views/receita.py
+++ receitas/views/receita.py
@@ -5,9 +5,11 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
-
+    paginator = Paginator(receitas, 3)
+    page = request.GET.get('page')
+    receitas_por_pagina = paginator.get_page(page)
     dados = {
-      'receitas' : receitas
+      'receitas' : receitas_por_pagina
     }
     
     return render(request ,'index.html', dados)
